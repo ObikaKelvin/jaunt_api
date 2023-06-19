e463d5e99508c844150536216d0987643317dd15
obikakelvin <obikakelvin.2255@gmail.com>
2023-06-19 12:36:22 -0700

    feat: adds global error handler

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const errorController = require('./controllers/errorController');
+
 const app = express();
 
 // test get route
 app.get('/', (req, res) => res.status(200).json({
     message: "success"
 }))
+
+app.use(errorController)
 
 module.exports = app;
