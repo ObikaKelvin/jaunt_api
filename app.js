7c75102dc9ada2f708d092d0286d8131b4b02d60
obikakelvin <obikakelvin.2255@gmail.com>
2023-06-19 16:24:37 -0700

    chore: adds database connection

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
+
 const errorController = require('./controllers/errorController');
+
+const router = require('./routes');
+
+const AppError = require('./utils/appError');
+
 
 const app = express();
 
@@ -7,6 +13,8 @@ const app = express();
 app.get('/', (req, res) => res.status(200).json({
     message: "success"
 }))
+
+app.use('/api/v1', router);
 
 app.use('*', (req, res, next) => {
     next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
@@ -8,7 +9,34 @@ dotenv.config();
 // get port from env file
 const { PORT } = process.env;
 
+// connect to database
+let DB = process.env.DEV_DB_URI
+
+if(process.env.NODE_ENV === "production") {
+    DB = process.env.PROD_DB_URI.replace(
+        '<password>',
+        process.env.PROD_DB_PASSWORD
+    ); 
+}
+
 // listen to desired port
 app.listen(PORT, () => {
-    console.log(`App is running on ${PORT}`)
+    console.log(`App is running on ${PORT}`);
+    console.log(DB)
+
+    try {
+
+        mongoose.set('strictQuery', true)
+        mongoose.connect(
+            DB,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,                
+            }
+        ).then(() => console.log(" Database is connected"))
+        .catch(e => console.log(e))
+    
+    } catch (e) {
+        console.log("could not connect");
+    }
 })
