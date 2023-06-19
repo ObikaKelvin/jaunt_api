be9a904f218acb19f73431c50c35ac2c0838cdc5
obikakelvin <obikakelvin.2255@gmail.com>
2023-06-19 15:00:20 -0700

    feat: adds 404 route handler

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.get('/', (req, res) => res.status(200).json({
     message: "success"
 }))
 
+app.use('*', (req, res, next) => {
+    next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
 app.use(errorController)
 
 module.exports = app;
