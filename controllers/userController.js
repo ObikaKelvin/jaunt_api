d56559bd0471356946215f3f43ba823693127cc8
obikakelvin <obikakelvin.2255@gmail.com>
2023-06-22 21:21:38 -0700

    feat: adds verify contacts

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
 const User = require('../models/User');
 
 exports.getMe = catchAsync(
@@ -30,6 +31,12 @@ exports.verifyContacts = catchAsync(
      * 
      */
     async (req, res, next) => { 
+        const { contacts } = req.body;
+
+        if(!contacts || contacts.length === 0) {
+            return next(new AppError("Please provide a list of contacts", 400));
+        }
+
         const users = await User.find({
             'phoneNumber': { $in: contacts}
         }, {_id: 1,  phoneNumber: 1 });
