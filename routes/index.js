405a2d4cea3a289940a24b4fddf443afc3d1b9b9
obikakelvin <obikakelvin.2255@gmail.com>
2023-08-02 19:48:02 -0700

    chore: refactors code

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const authRoute = require('./authRoute');
 const userRoute = require('./userRoute');
-const groupRoute = require('./groupRoute');
 const activityRoute = require('./activityRoute');
 const preferenceRoute = require('./preferenceRoute');
 const notificationRoute = require('./notificationRoute');
@@ -10,7 +9,6 @@ const router = express.Router({ mergeParams: true });
 
 router.use('/auth', authRoute);
 router.use('/users', userRoute);
-router.use('/groups', groupRoute);
 router.use('/activities', activityRoute);
 router.use('/preferences', preferenceRoute);
 router.use('/notifications', notificationRoute);
